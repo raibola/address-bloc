fd2de1f78a3b690dd3419cac4a9fb9a4456e7a91
Raissa Bola <raissabola@gmail.com>
2019-02-10 21:37:53 -0800

    Checkpoint Database: Completed

---
+++ controllers/ContactController.js
@@ -0,0 +1,13 @@
+const inquirer = require("inquirer");
+const Contact = require("../db/models").Contact;
+
+module.exports = class ContactController {
+
+  constructor(){
+      this.contacts = [];
+  }
+
+  addContact(name, phone){
+    return Contact.create({name, phone})
+  }
+}
