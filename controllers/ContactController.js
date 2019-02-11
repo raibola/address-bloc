14ce48de2a354acaae2239c60f25945abdc967c5
Raissa Bola <raissabola@gmail.com>
2019-02-10 23:18:19 -0800

    Assignment Database: Completed

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -5,9 +5,35 @@ module.exports = class ContactController {
 
   constructor(){
       this.contacts = [];
+      this.addContactQuestions = [
+        {
+          type: "input",
+          name: "name",
+          message: "Contact's name - ",
+          validate(val){
+            return val !== "";
+          }
+        },
+        {
+          type: "input",
+          name: "phone",
+          message: "Contact's phone number - ",
+          validate(val){
+            return val !== "";
+          }
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "Contact's email address - ",
+          validate(val){
+            return val !== "";
+          }
+        }
+      ];
   }
 
-  addContact(name, phone){
-    return Contact.create({name, phone})
+  addContact(name, phone, email){
+    return Contact.create({name, phone, email})
   }
 }
